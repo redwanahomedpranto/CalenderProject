1fde5f1943b212f6b9873241b79f5365c3e3dd40
redwanahomedpranto <157335241+redwanahomedpranto@users.noreply.github.com>
2024-02-04 17:49:34 +0600

    Add files via upload

---
+++ Calender2/src/main/java/main/Cell.java
@@ -0,0 +1,12 @@
+
+package main;
+
+import javax.swing.JButton;
+
+public class Cell extends JButton {
+    
+    public Cell(){
+       setContentAreaFilled(false);
+       setBorder(null);
+    }
+}
